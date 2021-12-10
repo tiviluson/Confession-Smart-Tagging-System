7ba0b6d484e5f61ae7f4dbbeca0775a9628e6b80
NguyenThienAn0610 <68161545+NguyenThienAn0610@users.noreply.github.com>
2021-12-10 18:12:54 +0700

    Add example data and refactor predict_tag.py

--- predict_tag.py
+++ predict_tag.py
@@ -5,7 +5,7 @@ import pickle
 from os import listdir
 
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer, tokenizer_from_json
 from keras.preprocessing.sequence import pad_sequences
 
 import sys
@@ -16,12 +16,14 @@ from keras.layers import LSTM, Dense, Embedding
 
 from underthesea import word_tokenize
 
-sep = os.sep # directory separtor
+sep = os.sep # directory separator
 data_folder = "data" # folder that contains data and model
 data_file = "Data.csv"
-model_version = "7"
+model_version = "example"
 
-enable_train_new_model = True # Set this to False if you want to reuse an existing model with model_version
+enable_train_new_model = False # Set this to False if you want to reuse an existing model with model_version
+
+PAD_LEN = 500 # The maximum length of a sentence
 
 # Dictionary to scale the dataset for a more balanced dataset
 freq = dict({("#tìmngườiyêu", 4), ("#lcd", 9), ("#gópý", 11), ("#bócphốt", 12), ("#hỏiđáp", 13), ("#tìmbạn", 23), ("#tâmsự", 1), ("#chiasẻ", 1)})
@@ -42,13 +44,27 @@ def txtTokenizer(texts):
     word_index = tokenizer.word_index
     return tokenizer, word_index
 
+# def preProcess(sentences):
+#     # sentences: The list of all sentences in a confession
+#     text = [word_tokenize(sentence, format="text") for sentence in sentences]
+# #     text = [re.sub(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", '', sentence) for sentence in sentences if sentence!='']
+#     text = [re.sub(r'([^\s\w]|"\')+', '', sentence) for sentence in text if sentence!='']
+#     text = [sentence.lower().strip().split() for sentence in text]
+#     return text
+
 # Remove trash symbols and spaces + Lower the case of all data
 def preProcess(sentences):
-    # sentences: The list of all sentences in a confession
-    text = [word_tokenize(sentence, format="text") for sentence in sentences]
-#     text = [re.sub(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", '', sentence) for sentence in sentences if sentence!='']
-    text = [re.sub(r'([^\s\w]|"\')+', '', sentence) for sentence in text if sentence!='']
-    text = [sentence.lower().strip().split() for sentence in text]
+    # Split sentences according to ; * \n . ? !
+    text = re.split('; |\*|\n|\.|\?|\!', sentences)
+
+    # Remove the " \ /
+    text = [re.sub(r'|"|\\|\/', '', sentence) for sentence in text]
+
+    # VNmese compound noun
+    text = [word_tokenize(sentence, format="text") for sentence in text]
+
+    # lowercase everything and remove all unnecessary spaces
+    text = [sentence.lower().strip().split() for sentence in text if sentence != '']
     return text
 
 # Pre-process tags to become lowercase and standardize them into 8 categories
@@ -79,8 +95,7 @@ def loadData(df):
     texts = []
     labels = []
     for sample in df['content']:
-        sentences = sample.split('.')
-        sentences = preProcess(sentences)
+        sentences = preProcess(sample)
         tag = df.loc[df.content == sample, 'tag'].values[0]
         label = [preProcessTag(tag) for _ in sentences]
         # [tag tag tag tag tag]
@@ -89,71 +104,92 @@ def loadData(df):
             labels = labels + label
     return texts, labels
 
-if __name__ == '__main__':
+# Train an entirely new model and save it
+def trainData():
     dataframe = loadDataFromCSV()
     texts, labels = loadData(dataframe)
     tokenizer, word_index = txtTokenizer(texts)
 
-    # put the tokens in a matrix
-    X = tokenizer.texts_to_sequences(texts)
-    X = pad_sequences(X, maxlen=500)
+    # Save the tokenizer
+    json_tokenizer = tokenizer.to_json()
+    json_file = open(data_folder + sep + "tokenizer_" + model_version + ".json", "w")
+    json_file.write(json_tokenizer)
+    json_file.close()
 
-    # prepare the labels
+    # Put the tokens in a matrix
+    X = tokenizer.texts_to_sequences(texts)
+    X = pad_sequences(X, maxlen=PAD_LEN)
+
+    # Prepare the labels
     Y = pd.get_dummies(labels)
 
-    if enable_train_new_model:
-        # Save the new model
-        file = open(data_folder + sep + "data_" + model_version + ".pkl", 'wb')
-        pickle.dump([X,Y, texts],file)
-        file.close()
-    else:
-        file = open(data_folder + sep + "data_" + model_version + ".pkl", 'rb')
-        X, Y, texts = pickle.load(file)
-        file.close()
+    # Save the new model
+    file = open(data_folder + sep + "data_" + model_version + ".pkl", 'wb')
+    pickle.dump([X,Y, texts],file)
+    file.close()
+
+    # Train Word2Vec model on our data
+    word_model = gensim.models.Word2Vec(texts, vector_size=300, min_count=1, epochs=10)
+    word_model.save(data_folder + sep + "word_model_" + model_version + ".save")
+
+    embedding_matrix = np.zeros((len(word_model.wv) + 1, 300))
+    for i, vec in enumerate(word_model.wv.vectors):
+        embedding_matrix[i] = vec
 
     # Split train an test sets
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, shuffle=True)
 
-    # train Word2Vec model on our data
-    if enable_train_new_model:
-        word_model = gensim.models.Word2Vec(texts, vector_size=300, min_count=1, epochs=10)
-        word_model.save(data_folder + sep + "word_model_" + model_version + ".save")
+    model = Sequential()
+    model.add(Embedding(len(word_model.wv)+1,300,input_length=X.shape[1],weights=[embedding_matrix],trainable=False))
+    model.add(LSTM(300,return_sequences=False))
+    model.add(Dense(Y.shape[1],activation="softmax"))
+    model.summary()
+    model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=['acc'])
 
-        embedding_matrix = np.zeros((len(word_model.wv) + 1, 300))
-        for i, vec in enumerate(word_model.wv.vectors):
-            embedding_matrix[i] = vec
-
-        model = Sequential()
-        model.add(Embedding(len(word_model.wv)+1,300,input_length=X.shape[1],weights=[embedding_matrix],trainable=False))
-        model.add(LSTM(300,return_sequences=False))
-        model.add(Dense(Y.shape[1],activation="softmax"))
-        model.summary()
-        model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=['acc'])
-
-        batch = 64
-        epochs = 1
-        model.fit(X_train,Y_train,batch,epochs)
-        model.save(data_folder + sep + "predict_model_" + model_version + ".save")
-    else:
-        word_model = gensim.models.Word2Vec.load(data_folder + sep + "word_model_" + model_version + ".save")
-        model = load_model(data_folder + sep + "predict_model_" + model_version + ".save")
-        model.summary()
-
+    batch = 64
+    epochs = 1
+    model.fit(X_train,Y_train,batch,epochs)
+    model.save(data_folder + sep + "predict_model_" + model_version + ".save")
     model.evaluate(X_test, Y_test)
 
+    return tokenizer, model, Y_train.columns
+
+# Reload all required data for reusing a model
+def reloadData():
+    file = open(data_folder + sep + "tokenizer_" + model_version + ".json")
+    tokenizer = tokenizer_from_json(file.read())
+    file = open(data_folder + sep + "data_" + model_version + ".pkl", 'rb')
+    X, Y, texts = pickle.load(file)
+    file.close()
+    # Split train an test sets
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, shuffle=True)
+    # word_model = gensim.models.Word2Vec.load(data_folder + sep + "word_model_" + model_version + ".save")
+    model = load_model(data_folder + sep + "predict_model_" + model_version + ".save")
+    model.summary()
+    return tokenizer, model, Y_train.columns
+
+def predictTag(tokenizer, model, labels):
     # Test model
     file = open(data_folder + sep + "My_data.txt", "r", encoding="utf8")
-    input_string = file.read().split('.')
+    input_string = file.read()
     X_dev = tokenizer.texts_to_sequences(preProcess(input_string))
-    print(tokenizer.sequences_to_texts(X_dev))
-    X_dev = pad_sequences(X_dev, maxlen=len(X_test[0]))
+    X_dev = pad_sequences(X_dev, maxlen=PAD_LEN)
     print("Predicting...")
     result_prediction_dict = dict()
     prediction_cus = model.predict(X_dev, verbose=0)
     print(tokenizer.sequences_to_texts(X_dev))
     for i in range(len(prediction_cus)):
-        result_tag = Y_train.columns[np.argmax(prediction_cus[i])]
-        # print(result_tag)
+        result_tag = labels[np.argmax(prediction_cus[i])]
         result_prediction_dict[result_tag] = result_prediction_dict.get(result_tag, 0) + 1
     print(result_prediction_dict)
     print(max(zip(result_prediction_dict.values(), result_prediction_dict.keys()))[1])
+
+if __name__ == '__main__':
+    tokenizer = None
+    model = None
+    labels = None
+    if enable_train_new_model:
+        tokenizer, model, labels = trainData()
+    else:
+        tokenizer, model, labels = reloadData()
+    predictTag(tokenizer, model, labels)
